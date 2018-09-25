7b95c42b1220e451db4837e052f6e99ff75bd6a5
Dugagjin Lashi <lashi.dugagjin@gmail.com>
2018-09-25 23:15:07 +0200

    refractor

--- source/index.ts
+++ source/index.ts
@@ -1,11 +1,8 @@
-const throwTypes = ['object', 'undefined', 'function', 'symbol'];
+type T = number | string | boolean;
 
-export function encode<T>(input: T[]): [number, T][] {
-    // check if array empty and if element is comparable
-    const isInputInvalid =
-        input.length === 0 || input.some(e => throwTypes.includes(typeof e));
-    if (isInputInvalid) {
-        throw new Error(`Input not valid`);
+export function encode(input: T[]): [number, T][] {
+    if (input.length === 0) {
+        throw new Error(`array empty`);
     }
     const encoding: [number, T][] = [];
     let prev = input[0];
@@ -23,14 +20,9 @@ export function encode<T>(input: T[]): [number, T][] {
     return encoding;
 }
 
-export function decode<T>(input: [number, T][]): T[] {
-    // check if array is empty, sub arrays are length 2
-    // and if second element comparable
-    const isInputInvalid =
-        input.length === 0 ||
-        input.some(e => !(e.length === 2) && throwTypes.includes(typeof e[1]));
-    if (isInputInvalid) {
-        throw new Error(`Input not valid.`);
+export function decode(input: [number, T][]): T[] {
+    if (input.length === 0 || input.some(e => !(e.length === 2))) {
+        throw new Error(`input not valid.`);
     }
     return input
         .map(e => Array.from<number>({ length: e[0] }).map(() => e[1]))
